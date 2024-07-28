c1f7d90a63e7cce42ec32150cfa42b93f403d447
Jan <jan.j.klinger@gmail.com>
2024-07-28 23:05:46 +0200

    create service

---
+++ app/services/github.js
@@ -0,0 +1,21 @@
+import Service from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import fetch from 'fetch';
+
+export default class GithubService extends Service {
+  @tracked token;
+
+  setToken(token) {
+    this.token = token;
+  }
+
+  async fetchBranches(fullName) {
+    let response = await fetch(`https://api.github.com/repos/${fullName}/branches`, {
+      headers: {
+        'Authorization': `token ${this.token}`,
+        'Accept': 'application/vnd.github+json'
+      }
+    });
+    return await response.json();
+  }
+}
