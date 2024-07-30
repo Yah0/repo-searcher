483f09bbed6096d860b7205c905405e8c6e1cc31
Jan <jan.j.klinger@gmail.com>
2024-07-30 16:41:46 +0200

    refactor model

--- app/models/repository.js
+++ app/models/repository.js
@@ -6,8 +6,7 @@ export default class RepositoryModel extends Model {
   @attr htmlUrl;
   @attr language;
   @attr isPrivate;
-  @belongsTo('organization', { async: true, inverse: 'repositories' })
-  organization;
+  @belongsTo('organization', { async: true, inverse: 'repositories' }) organization;
 
   get fullName() {
     return `${this.owner}/${this.name}`;
